73005f42e7761646a174fa4c8eb6524027bfbe80
Zafeer <mohammadzafeer2610@gmail.com>
2024-07-30 14:02:17 +0530

    char array pointers

---
+++ charArrayPointers3.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int arr[5]={0,1,2,3,4};
+    char ch[6]="abcde";
+
+    cout<<arr<<endl;
+    cout<<ch<<endl;
+
+    return 0;
+}
--- pointers2.cpp
+++ pointers2.cpp
@@ -1,19 +1,48 @@
+// #include<iostream>
+// using namespace std;
+
+// int main()
+// {
+    // int arr[10]={2,4,6,8,10,12,14,16,18,20};
+
+    // cout<<"Address of first element of array is : "<<*arr<<endl;
+
+    // cout<<endl;
+    // cout<<arr<<endl;
+    // cout<<1[arr]<<endl;
+    //IMPORTANT POINT  ----->  arr[i] = i[arr] cuz ,==>  arr[i]=*(arr+i)
+    // cout<<endl;
+    // for (int i=0; i<10; i++){
+    //     cout<<"Address of "<<i+1<<"th element is "<<arr+i<<" and value is "<<*(arr+i)<<endl;
+    // }
+
+    // int temp[10]={1,2};
+    
+    // cout<<"Size of temp ("<<temp<<") is "<<sizeof(temp)<<endl;
+    // cout<<"Size of *temp ("<<*temp<<") is "<<sizeof(*temp)<<endl;
+    // cout<<"size of &temp ("<<&temp<<") is "<<sizeof(&temp)<<endl;
+
+    // int *ptr=&temp[0];
+    // cout<<"size of ptr ("<<ptr<<") is "<<sizeof(ptr)<<endl;
+    // cout<<"size of *ptr ("<<*ptr<<") is "<<sizeof(*ptr)<<endl;
+    // cout<<"size of &ptr ("<<&ptr<<") is "<<sizeof(&ptr)<<endl;
+
+    // return 0;
+// }
+
+
+
 #include<iostream>
 using namespace std;
 
 int main()
 {
-    int arr[10]={2,4,6,8,10,12,14,16,18,20};
+    int arr[10];
+    // arr=arr+1;  This Is Error
 
-    cout<<"Address of first element of array is : "<<*arr<<endl;
-
-    cout<<endl;
-    cout<<arr<<endl;
-    cout<<1[arr]<<endl;
-    //IMPORTANT POINT  ----->  arr[i] = i[arr] cuz ,==>  arr[i]=*(arr+i)
-    cout<<endl;
-    for (int i=0; i<10; i++){
-        cout<<"Address of "<<i+1<<"th element is "<<arr+i<<" and value is "<<*(arr+i)<<endl;
-    }
+    int *ptr=&arr[0];
+    cout<<ptr<<endl;
+    ptr=ptr+1;
+    cout<<ptr;
     return 0;
 }
